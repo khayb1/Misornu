362d7c0bc979eb1cb88310c2761c69d6ce725b67
Kelvin Afrane <shelvinbrown@gmail.com>
2025-04-15 10:29:04 +0000

    firebase error fix

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,4 @@
-// src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -11,14 +11,16 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 // Initialize Auth
 export const auth = getAuth(app);
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+console.log("API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
